bbbc74ad455c431176632a6d6a9d0bd7b365752b
Sidirius.Andyron@gmail.com <Sidirius.Andyron@gmail.com@4a4762a4-557e-15b0-90b7-621c9cb0b54e>
2012-09-21 10:20:45 +0000

    int = 70

--- trunk/Hello/Server.java
+++ trunk/Hello/Server.java
@@ -8,7 +8,7 @@ import java.util.Set;
 //a
 public class Server {
 
-  int i = 954;
+  int i = 70;
 	ServerSocketChannel ssc;
 	SocketChannel socketChannel;
 	ArrayList<SocketChannel> channels = new ArrayList<SocketChannel>();
