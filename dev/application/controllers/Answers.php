dde6b6653e4b25264e2c58026209335c9a8bee79
jursa@freevision.sk <jursa@freevision.sk@4a4762a4-557e-15b0-90b7-621c9cb0b54e>
2012-11-15 19:12:33 +0000

    Answers.php controller ...

---
+++ dev/application/controllers/Answers.php
@@ -0,0 +1,10 @@
+<?php if (!defined('APPPATH')) { die('No direct access allowed ...'); }
+
+class Answers extends Abstract_frontend_controller {
+
+    public function ajaxCheckAnswer($answer_id = NULL) {
+        $this->output->set_content_type('application/json');
+        $this->output->set_output(json_encode(true));
+    }	
+	
+}
