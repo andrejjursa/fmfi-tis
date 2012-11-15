11697aeacf0978a7198d4b9578d047da87079728
saamoot@gmail.com <saamoot@gmail.com@4a4762a4-557e-15b0-90b7-621c9cb0b54e>
2012-11-15 19:52:33 +0000

    test svn :)

--- dev/application/controllers/Answers.php
+++ dev/application/controllers/Answers.php
@@ -5,6 +5,9 @@ class Answers extends Abstract_frontend_controller {
     public function ajaxCheckAnswer($answer_id = NULL) {
         $this->output->set_content_type('application/json');
         $this->output->set_output(json_encode(true));
+        
+        
+        //test
     }	
 	
 }
