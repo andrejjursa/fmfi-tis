1c6c128f24b907e2fec1ca04de413dc5055a728b
saamoot@gmail.com <saamoot@gmail.com@4a4762a4-557e-15b0-90b7-621c9cb0b54e>
2012-11-15 20:28:50 +0000

    answer controler, db data fetch test

--- dev/application/controllers/Answers.php
+++ dev/application/controllers/Answers.php
@@ -3,15 +3,23 @@
 class Answers extends Abstract_frontend_controller {
 
     public function ajaxCheckAnswer($answer_id = NULL) {
-        //$this->output->set_content_type('application/json');
+        $this->output->set_content_type('application/json');
         if ($answer_id = NULL) {
             $this->output->set_output(json_encode(false));
         }
         
         $answer = $this->load->table_row("answers");
-        $bla = $answer->load($answer_id);
+        if ($answer->load($answer_id)) {
+            $odpoved = $answer->getCorrect();
+        } else {
+            $this->output->set_output(json_encode(false));
+        }
         
-        var_dump($answer, $bla);
+        if ($odpoved === 1) {
+            $this->output->set_output(json_encode(true));
+        } else {
+            $this->output->set_output(json_encode(false));
+        }
     }	
 	
 }
