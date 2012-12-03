0350187fec527018b2241664b7b1c7755c4be193
riso37@gmail.com <riso37@gmail.com@4a4762a4-557e-15b0-90b7-621c9cb0b54e>
2012-12-03 11:37:17 +0000

    006_base_tables added...

---
+++ dev/application/migrations/006_base_tables.php
@@ -0,0 +1,69 @@
+<?php if (!defined('APPPATH')) { die('No direct access allowed ...'); }
+    
+class Migration_Base_tables extends CI_Migration {
+    
+    public function up() {
+        /**
+         * Add periods table begin.
+         */
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => '11',
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE,
+            ),              
+            'tstamp' => array(
+                'type' => 'timestamp',
+            ),
+            'crdate' => array(
+                'type' => 'timestamp',
+            ),
+            'name' => array(
+                'type' => 'varchar',
+                'constraint' => '255',
+                'default' => '',
+                'null' => FALSE,
+            ),
+            'description' => array(
+                'type' => 'TEXT',
+				'default' => '',
+                'null' => TRUE,
+            ),
+            'photo' => array(
+                'type' => 'INT',
+                'constraint' => '11',
+                'default' => '0',
+            ),
+            'start_year' => array(
+                'type' => 'int',
+                'constraint' => '4',
+                'default' => '0',
+            ),
+            'end_year' => array(
+                'type' => 'int',
+                'constraint' => '4',
+                'default' => '9999',
+            ),			
+        ));
+        
+        $this->dbforge->add_key('id', TRUE);
+        
+        $this->dbforge->create_table('periods');
+        /**
+         * Add periods table end.
+         */
+
+    }
+    
+    public function down() {
+        /**
+         * Drop periods table.
+         */
+        $this->dbforge->drop_table('periods');
+
+    }
+    
+}
+    
+?>
