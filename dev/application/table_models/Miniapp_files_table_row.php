5e7f93105fcaf717a5bcf6c1e80facaaddbb295b
jursa@freevision.sk <jursa@freevision.sk@4a4762a4-557e-15b0-90b7-621c9cb0b54e>
2012-12-16 15:05:17 +0000

    Docs updated.

--- dev/application/table_models/Miniapp_files_table_row.php
+++ dev/application/table_models/Miniapp_files_table_row.php
@@ -1,5 +1,8 @@
 <?php if (!defined('APPPATH')) { die('No direct access allowed ...'); }
 
+/**
+ * @package TableModels
+ */
 class Miniapp_files_table_row extends Abstract_table_row {
     
 }
--- dev/application/table_models/Miniapps_table_row.php
+++ dev/application/table_models/Miniapps_table_row.php
@@ -1,5 +1,8 @@
 <?php if (!defined('APPPATH')) { die('No direct access allowed ...'); }
 
+/**
+ * @package TableModels
+ */
 class Miniapps_table_row extends Abstract_table_row {
     
     /**
