75b0a170c63a89a7a24adf983bc7d95853b9714a
technodrome1@azet.sk <technodrome1@azet.sk@4a4762a4-557e-15b0-90b7-621c9cb0b54e>
2012-12-17 16:35:25 +0000

    TinyMCE relative urls

--- dev/public/js/admin_editor/editor.js
+++ dev/public/js/admin_editor/editor.js
@@ -73,7 +73,9 @@ jQuery(document).ready(function($){
         theme_advanced_toolbar_align : 'left',
         theme_advanced_statusbar_location : 'bottom',
         theme_advanced_resizing: false,
-        
+        relative_urls : false,
+        remove_script_host : false,
+		
         onchange_callback: function(editor) {
             highlightChange();
             $('#' + editor.id).valid();
